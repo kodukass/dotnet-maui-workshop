d71437ece42a8a46e678c2c15c04d14fdd04569b
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-05-25 15:28:39 +0300

    part 1 valmis?

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -4,4 +4,16 @@ namespace MonkeyFinder.Model;
 
 public class Monkey
 {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public int Population { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+
+
 }
