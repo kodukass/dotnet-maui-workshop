e0bed345996e1693cc8f21d88498e2092a94c519
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-05-25 16:33:35 +0300

    hurray poole peal

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,52 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+//[INotifyPropertyChanged]
+public partial class BaseViewModel : ObservableObject//: INotifyPropertyChanged
 {
+    public BaseViewModel()
+    {
+        
+    }
+
+    [ObservableProperty]
+   // [AlsoNotifyChangeFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    //public bool IsBusy
+    //{
+    //    get => isBusy;
+    //    set
+    //    {
+    //        if (isBusy == value)
+    //            return;
+    //        isBusy = value;
+    //        OnPropertyChanged();
+    //        OnPropertyChanged(nameof(IsNotBusy));
+    //    }
+    //}
+
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title == value)
+    //            return;
+    //        title = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+
+    public bool IsNotBusy => !isBusy;
+
+    //public event PropertyChangedEventHandler PropertyChanged;
+
+    //public void OnPropertyChanged([CallerMemberName]string name = null)
+    //{
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    //}
 }
